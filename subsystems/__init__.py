dea2c79b90a94010e0753017513cfc917e0a5b60
Caden Dalley <caden.dalley@icloud.com>
2026-02-03 23:20:44 -0700

    Revamp StateSubsystem

--- subsystems/__init__.py
+++ subsystems/__init__.py
@@ -1,149 +1,114 @@
-from abc import ABC, ABCMeta
-from enum import Enum
+"""StateSubsystem class for simple state subsystems."""
+from enum import Enum, EnumMeta
 
-from commands2 import Command, InstantCommand
+from commands2 import Command
 from commands2.subsystem import Subsystem
-from ntcore import *
-from phoenix6 import utils
-from phoenix6.hardware import TalonFX
-from wpilib import RobotController, DriverStation
-from wpilib.simulation import DCMotorSim
-from wpimath import units
-from wpimath.system.plant import DCMotor, LinearSystemId
+from pykit.logger import Logger
 
 
-class StateSubsystemMeta(ABCMeta, type(Subsystem)):
-    pass
-
-
-class StateSubsystem(Subsystem, ABC, metaclass=StateSubsystemMeta):
+class StateSubsystem(Subsystem):
     """
-    Subsystem class for handling subsystem state transitions and motor sim models.
+    Subsystem class for handling subsystem state transitions.
+    Updated for 2026 to utilize PyKit.
     """
 
     class SubsystemState(Enum):
-        OFF = 0
+        """
+        Possible state subsystem states.
+        Subclasses should extend this enum.
+        """
+        OVERRIDE_THIS_ENUM = None
 
     def __init__(self, name: str, starting_state: SubsystemState):
         """
-        Initializes all subsystem logging and sets the default state.
-        
+        Sets the default state of the subsystem to starting_state.
+
+        Last state is also set to starting state, meaning `self.has_changed()` will not
+        trigger on __init__.
+
         :param name: Name of the subsystem
         :type name: str
         :param starting_state: Starting state of the subsystem
-        :type starting_state:
+        :type starting_state: SubsystemState
         """
         super().__init__()
         self.setName(name.title())
+        self._starting_state = starting_state
 
-        self._frozen = False
-        self._subsystem_state = None  # This allows set_desired_state to succeed
-        self.__starting_state = starting_state
+        if not hasattr(self.__class__, "SubsystemState"):
+            # Must have a SubsystemState
+            raise TypeError("Subsystems must define a SubsystemState.")
 
-        # Create NT folder for organization
-        self._network_table = NetworkTableInstance.getDefault().getTable(name.title())
-        self._nt_publishers = []
-        # current_state_nt = self._network_table.getStringTopic("Current State")
-        # self._current_state_pub = current_state_nt.publish()
-        # self._current_state_pub.set("INITIALIZING")
+        state_enum = self.__class__.SubsystemState
+        if not isinstance(state_enum, EnumMeta):
+            # SubsystemState needs to be some sort of Enum
+            raise TypeError(
+                f"{self.__class__.__name__}.SubsystemState must be an Enum."
+            )
 
-        # frozen_nt = self._network_table.getBooleanTopic("Frozen")
-        # self._frozen_pub = frozen_nt.publish()
-        # self._frozen_pub.set(self._frozen)
+        if "OVERRIDE_THIS_ENUM" in state_enum.__members__:
+            # Subclasses must implement their own SubsystemState
+            raise TypeError("Subsystems must override SubsystemState.")
 
-        self._sim_models: list[tuple[DCMotorSim, TalonFX]] = []
-
-    def set_desired_state(self, desired_state: SubsystemState) -> bool:  # type: ignore
-        """
-        Sets the desired state of the subsystem.
-        It's recommended to override this function in order to update objects such as control requests.
-        """
-        current_state = self._subsystem_state
-        if current_state is desired_state or DriverStation.isTest() or self.is_frozen():
-            return False
-        self._subsystem_state = desired_state
-        # self._current_state_pub.set(self.get_state_name())
-        return True
+        self._locked = False
+        self._last_state = self._subsystem_state = starting_state
+        self._has_changed = False
 
     def periodic(self):
-        # We call set_desired_state once in periodic to ensure the state is correctly set on startup
-        if self._subsystem_state is None:
-            self.set_desired_state(self.__starting_state)
-        if not utils.is_simulation():
-            return
-        for model in self._sim_models:
-            sim = model[1].sim_state
-            sim.set_supply_voltage(RobotController.getBatteryVoltage())
-            model[0].setInputVoltage(sim.motor_voltage)
-            model[0].update(0.02)
+        # Ensure the state is correctly set on startup
 
-            sim.set_raw_rotor_position(
-                units.radiansToRotations(model[0].getAngularPosition())
-                * model[0].getGearing()
-                )
-            sim.set_rotor_velocity(
-                units.radiansToRotations(model[0].getAngularVelocity())
-                * model[0].getGearing()
-                )
-            sim.set_rotor_acceleration(
-                units.radiansToRotations(model[0].getAngularAcceleration())
-                * model[0].getGearing()
-                )
+        if self._last_state != self._subsystem_state:
+            self._has_changed = True
+        else:
+            self._has_changed = False
+        self._last_state = self._subsystem_state
 
-    def freeze(self) -> None:
-        """Prevents new state changes."""
-        self._frozen = True
-        # self._frozen_pub.set(True)
+        # PyKit logging
+        Logger.recordOutput(
+            f"{self.getName()}/Subsystem State",
+            self._subsystem_state.name
+        )
+        Logger.recordOutput(f"{self.getName()}/Has Changed", self._has_changed)
+        Logger.recordOutput(f"{self.getName()}/Locked", self._locked)
+        Logger.recordOutput(f"{self.getName()}/Last State", self._last_state.name)
 
-    def unfreeze(self) -> None:
+    def lock_state(self) -> None:
+        """Prevents state changes."""
+        self._locked = True
+
+    def unlock_state(self) -> None:
         """Allows state changes."""
-        self._frozen = False
-        # self._frozen_pub.set(False)
+        self._locked = False
 
-    def is_frozen(self) -> bool:
-        return self._frozen
+    def is_locked(self) -> bool:
+        """Returns True if the subsystem is locked."""
+        return self._locked
+
+    def is_unlocked(self) -> bool:
+        """Returns True if the subsystem is not locked."""
+        return not self._locked
+
+    def has_state_changed(self) -> bool:
+        """Returns True if the subsystem state has changed since the last loop."""
+        return self._has_changed
+
+    def on_state_change(self, old: SubsystemState, new: SubsystemState) -> None:
+        """Called when the state changes. Override if needed."""
+
+    def get_last_state(self) -> SubsystemState:
+        """Returns the last state of the subsystem."""
+        return self._last_state
 
     def get_current_state(self) -> SubsystemState | None:
-        state = self._subsystem_state
-        return state
+        """Returns the current state of the subsystem."""
+        return self._subsystem_state
 
-    def get_state_name(self) -> str:
-        """Returns the name of the current state."""
-        return self._subsystem_state.name.title().replace("_", " ")
-
-    def get_network_table(self) -> NetworkTable:
-        return self._network_table
-
-    def _add_talon_sim_model(self, talon: TalonFX, motor: DCMotor, gearing: float,
-                             moi: float = 0.001
-                             ) -> None:
-        """Creates a DCMotorSim that updates periodically during 
-        simulation. This also logs the talon's status signals to Network
-        Tables, regardless if simulated.
-
-        :param talon: The TalonFX to simulate.
-        :type talon: TalonFX
-        :param motor: The motor of the TalonFX on the physical robot.
-        :type motor: DCMotor
-        :param gearing: The gearing from the TalonFX to the mechanism.
-        This will depend on whether the mechanism is relying on the
-        motor's internal sensor or an absolute encoder.
-        :type gearing: float
-        :param moi: The moment of interia for the simulated mechanism, 
-        defaults to 0.001
-        :type moi: float, optional
-        """
-        self._sim_models.append(
-            (DCMotorSim(
-                LinearSystemId.DCMotorSystem(
-                    motor,
-                    moi,
-                    gearing
-                ),
-                motor
-            ),
-             talon)
-        )
+    def set_desired_state(self, desired_state: SubsystemState) -> None:
+        """Sets the desired state of the subsystem."""
+        if not self.is_locked() and desired_state != self._subsystem_state:
+            self._subsystem_state = desired_state
+            self.on_state_change(self._last_state, desired_state)
 
     def set_desired_state_command(self, state: SubsystemState) -> Command:
-        return InstantCommand(lambda: self.set_desired_state(state))
+        """Sets the desired state of the subsystem with an InstantCommand."""
+        return self.runOnce(lambda: self.set_desired_state(state))
